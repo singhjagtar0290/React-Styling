ece7800558e5c738d673c570da551e98c88302af
jagtar <singhjagtar0290@gmail.com>
2022-05-07 12:34:32 +0530

    Style components & Css approach

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-import './Button.css';
+import Style from './Button.module.css';
 
 const Button = props => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button type={props.type} className={Style.button} onClick={props.onClick}>
       {props.children}
     </button>
   );
